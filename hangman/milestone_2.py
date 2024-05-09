27d479f7a53f559c4f38d43b2f652104c52a2284
dee25v <145212555+dee25v@users.noreply.github.com>
2024-05-09 21:26:50 +0100

    Update milestone_2.py

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -5,3 +5,23 @@ word_list = ["Apple", "Banana", "Mango", "Orange", "Grape"]
 
 #Step 3: Print out the newly created list to the standard output (screen)
 print("My favourite fruits are:" , word_list)
+
+import random
+
+# Step 1-3: Create a list containing the names of your 5 favorite fruits
+# and randomly select a word from the list
+word_list = ["Apple", "Banana", "Mango", "Orange", "Grapes"]
+word = random.choice(word_list)
+
+# Step 4: Assign the randomly generated word to a variable called word
+
+# Step 5: Print out word to the standard output
+print("Randomly selected word:", word)
+
+# Step 1: Create a list containing the names of your 5 favourite fruits
+word_list = ["Apple", "Banana", "Mango", "Orange", "Grape"]
+
+# Step 2: Assign this list to a variable called word_list (already done in Step 1)
+
+#Step 3: Print out the newly created list to the standard output (screen)
+print("My favourite fruits are:" , word_list)
